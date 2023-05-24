80e2d78eb009d7481a8ad2cdf7d686346da45376
Vasili <Wasyok.ru@yandex.ru>
2023-05-24 21:08:38 +0500

    arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,88 @@
-﻿int a = 10;
-int b = 15;
-int ?c = 20;
-bool d = a > b;
-Console.WriteLine(d);
+﻿// int a = 10;
+// int b = 15;
+// int ?c = 20;
+// bool d = a > b;
+// Console.WriteLine(d);
+
+// Логические операции
+// bool a = (10<15) | (4>10);
+// bool b = (10>15) | (4>10);
+// Console.WriteLine((a));
+// Console.WriteLine((b));
+
+// Конструкция if..else и тернарная операция
+// int a = 10;
+// int b = 20;
+// if (a > b)
+// {
+//     Console.WriteLine($"number {a} > number {b}");
+// }else if (a !=b)
+// {
+//     Console.WriteLine(($"не равны{a} и {b}"));
+// }
+// else
+// {
+//     Console.WriteLine("ты дурак, схавал?");
+// } 
+
+// int v = 10;
+// int z = 5;
+// string i = v > z ? "lox" : "krasava";
+// Console.WriteLine((i));
+
+// Циклы
+
+// for (int i = 0; i <= 4; i++)
+// {
+//     Console.WriteLine((i));
+// }
+
+// var a = 1;
+// for (Console.WriteLine("begin cycles"); a < 4; Console.WriteLine($"a={a}"))
+// {
+//     a++;
+// }
+//
+// for (int i = 1, j = 10; i < 100; i++, j++)
+// {
+//     Console.WriteLine($"{i*j}");
+// }
+// int  i = 6;
+// do
+// {
+//     Console.WriteLine(i);
+//     i--;
+// } while (i > 0);
+
+// int i = 1;
+// while (i < 10)
+// {
+//     Console.WriteLine((i));
+//     i++;
+// }
+// foreach(var c in "Vasya"){
+//     Console.WriteLine((c));
+// }
+//
+// for (int i = 1; i < 10; i++)
+// {
+//     if(i>5) break;
+//     Console.WriteLine(i);
+// }
+// for (int i = 10; i < 100; i++)
+// {
+//     if(i % 2!=0)continue;
+//     Console.WriteLine((i));
+// }
+// for (int i = 1; i < 10; i++)
+// {
+//     for (int j = 1; j < 10; j++)
+//     {
+//         Console.Write($"{i*j}\t");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Массивы
+int[] arr = new int[4];
